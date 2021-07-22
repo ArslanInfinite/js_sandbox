2bb811cf5dd6976b4048fd5a7db08db0d52ebfb9
Arslan <ali.arslan319@gmail.com>
2021-07-22 01:30:02 -0400

    console log, table, error, warn and other practice

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert('Hello, world')
+console.log('Hello World')
+console.log(123)
+console.log(true)
+
+let greeting = 'Hello'
+console.log(greeting)
+console.table({a:1, b:2, c:3})
+
+console.error('This is an error')
+console.clear 
+console.warn('This is a warning')
+
+console.time('Total Time')
+    console.log('Hello World')
+    console.log('Hello World')
+    console.log('Hello World')
+    console.log('Hello World')
+    console.log('Hello World')
+console.timeEnd('Total Time')
+
